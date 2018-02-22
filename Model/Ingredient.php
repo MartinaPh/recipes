02fe4a32687086adacace427c24ba4dd5bc0504c
Martina Philippi <martina_philippi@hotmail.com>
2018-02-22 22:57:41 +0100

    Code korrigiert

--- Model/Ingredient.php
+++ Model/Ingredient.php
@@ -2,42 +2,92 @@
 
 class Ingredient 
 {
-    
+    /**
+     * Type of the ingredient.
+     * 
+     * @var type 
+     */
 private $type;
 
+    /**
+     * Unity of the quantity of the ingredient.
+     * 
+     * @var type 
+     */
 private $unity;
 
+    /**
+     * Quantity of the ingredient.
+     * 
+     * @var type 
+     */
 private $quantity;
 
-
-public function getType()    
+    /**
+     * Returns the type.
+     * 
+     * @return string
+     */
+     
+public function getType(): string    
 {
  return $this->type;  
 }
 
-public function setType($type)       
+    /**
+     * Sets the type.
+     * 
+     * @param string $type
+     */
+public function setType(string $type): self       
 {
 $this->type = $type;
-}
 
-public function getUnity()
+return $this;
+}
+    
+    /**
+     * Returns the unity.
+     * 
+     * @return string
+     */
+public function getUnity(): string
 {
     return $this->unity;
 } 
 
-public function setUnity($unity)
+    /**
+     * Sets the unity.
+     * 
+     * @param string $unity
+     */
+public function setUnity(string $unity): self
 {
     $this->unity = $unity;
+    
+    return $this;
 }
 
-public function getQuantity()
+    /**
+     * Returns the quantity.
+     * 
+     * @return int
+     */
+public function getQuantity(): int
 {
     return $this->quantity;
 }
 
-public function setQuantity($quantity)
+    /**
+     * Sets the quantity.
+     * 
+     * @param int $quantity
+     */
+public function setQuantity(int $quantity): self
 {
     $this->quantity = $quantity;
+    
+    return $this;
 }
 
 
